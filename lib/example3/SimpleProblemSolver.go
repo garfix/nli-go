6c95c1a4cf7156dc86ae1f3e75d44571a768fc48
Patrick van Bergen <patrick.vanbergen@gmail.com>
2016-09-25 21:39:33 +0200

    More problem solver

--- lib/example3/SimpleProblemSolver.go
+++ lib/example3/SimpleProblemSolver.go
@@ -62,20 +62,73 @@ func (solver simpleProblemSolver) SolveSingleRelationMultipleBindings(goalRelati
 	return newBindings
 }
 
-// goal e.g. father(Y, Z)
-// bindings e.g. {
-//  { {X='john', Y='jack'} }
-//  { {X='bob', Y='jonathan'} }
+// goalRelation e.g. father(Y, Z)
+// binding e.g. { X='john', Y='jack' }
+// return e.g. {
+//  { {X='john', Y='jack', Z='joe'} }
+//  { {X='bob', Y='jonathan', Z='bill'} }
 // }
-// return e.g. {X='bob', Y='jonathan', Z='bill'}
-func (solver simpleProblemSolver) SolveSingleRelationSingleBinding(goalRelation SimpleRelation, binding SimpleBinding) SimpleBinding {
+func (solver simpleProblemSolver) SolveSingleRelationSingleBinding(goalRelation SimpleRelation, binding SimpleBinding) []SimpleBinding {
 
-	bindings := []SimpleBinding{}
+	newBindings := []SimpleBinding{}
+
+	boundRelation := solver.matcher.bindSingleRelationSingleBinding(goalRelation, binding)
 
 	// go through all knowledge sources
 	for _, source := range solver.sources {
-		bindings = append(bindings, source.Bind(goalRelation))
+		newBindings = append(newBindings, solver.SolveSingleRelationSingleBindingSingleSource(boundRelation, binding, source))
+	}
+
+	return newBindings
+}
+
+// boundRelation e.g. father('jack', Z)
+// binding e.g. { X='john', Y='jack' }
+// return e.g. {
+//  { {X='john', Y='jack', Z='joe'} }
+//  { {X='bob', Y='jonathan', Z='bill'} }
+// }
+func (solver simpleProblemSolver) SolveSingleRelationSingleBindingSingleSource(boundRelation SimpleRelation, binding SimpleBinding, source SimpleKnowledgeBase) []SimpleBinding {
+
+	newBindings := []SimpleBinding{}
+
+	// boundRelation e.g. father(X, 'john')
+	// subgoalSets e.g. {
+	//    { male(X), parent(X, 'john') },
+	//    { child('john', X), male(X) }
+	// }
+	// bindings e.g. {
+	//    { X='Jack' },
+	// }
+	// Note: bindings are linked to subgoalSets, one on one; but usually just one of the arrays is used
+	sourceSubgoalSets, sourceBindings := source.Bind(boundRelation)
+
+	for i, sourceSubgoalSet := range sourceSubgoalSets {
+		sourceBinding := sourceBindings[i]
+
+		combinedBinding := binding.Merge(sourceBinding)
+
+		subgoalSetBindings := solver.SolveMultipleRelationsSingleBinding(sourceSubgoalSet, combinedBinding)
+		newBindings = append(newBindings, subgoalSetBindings)
+	}
+
+	return newBindings
+}
+
+// goal e.g. { father(X, Y), father(Y, Z)}
+// bindings {X='john', Y='jack'}
+// return e.g. {
+//  { {X='john', Y='jack', Z='joe'} }
+//  { {X='bob', Y='jonathan', Z='bill'} }
+// }
+func (solver simpleProblemSolver) SolveMultipleRelationsSingleBinding(goals []SimpleRelation, binding SimpleBinding) []SimpleBinding {
+
+	bindings := []SimpleBinding{}
+
+	for _, goal := range goals {
+		bindings = solver.SolveSingleRelationSingleBinding(goal, bindings)
 	}
 
 	return bindings
+
 }

96f43261a41d3946776a43e85731573d57b4c260
Patrick van Bergen <patrick.vanbergen@gmail.com>
2016-10-18 08:29:26 +0200

    RelationToSetMatcher tests

--- tests/Matcher_test.go
+++ tests/Matcher_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestMatchTwoRelations(t *testing.T) {
-	var tests = []struct {
+
+	parser := importer.NewInternalGrammarParser()
+	matcher := mentalese.NewRelationMatcher()
+	tests := []struct {
 		needle string
 		haystack string
 		binding string
@@ -23,9 +26,6 @@ func TestMatchTwoRelations(t *testing.T) {
 		{"parent(X, Y)", "parent('Luke', 'George')", "{X: 'Vincent'}", "{X: 'Vincent'}", false},
 	}
 
-	parser := importer.NewInternalGrammarParser()
-	matcher := mentalese.NewRelationMatcher()
-
 	for _, test := range tests {
 
 		needle, _ := parser.CreateRelation(test.needle)
@@ -38,6 +38,49 @@ func TestMatchTwoRelations(t *testing.T) {
 
 		if !resultBinding.Equals(wantBinding) || resultMatch != wantMatch {
 			t.Errorf("MatchTwoRelations(%v %v %v): got %v %v, want %v %v", needle, haystack, binding, resultBinding, resultMatch, wantBinding, wantMatch)
+		}
+	}
+}
+
+func TestMatchRelationToSet(t *testing.T) {
+
+	parser := importer.NewInternalGrammarParser()
+	matcher := mentalese.NewRelationMatcher()
+	haystack, _, _ := parser.CreateRelationSet("[gender('Luke', male) gender('George', male) parent('Luke', 'George') parent('Carry', 'Steven') gender('Carry', female)]")
+
+	var tests = []struct {
+		needle       string
+		haystack     mentalese.RelationSet
+		binding      string
+		wantBindings string
+		wantIndexes  []int
+	} {
+		{"parent(X, Y)", haystack, "{}", "[{X:'Luke', Y:'George'} {X:'Carry', Y:'Steven'}]", []int{2, 3}},
+		{"parent(X, 'Henry')", haystack, "{}", "[]", []int{}},
+		{"parent(X, 'Steven')", haystack, "{}", "[{X:'Carry'}]", []int{3}},
+	}
+
+	for _, test := range tests {
+
+		needle, _ := parser.CreateRelation(test.needle)
+		binding, _ := parser.CreateBinding(test.binding)
+		wantBindings, _ := parser.CreateBindings(test.wantBindings)
+		wantIndexes := test.wantIndexes
+
+		resultBindings, resultIndexes := matcher.MatchRelationToSet(needle, haystack, binding)
+
+		bindingsOk := (len(wantBindings) == len(resultBindings))
+		for i, resultBinding := range resultBindings {
+			bindingsOk = bindingsOk && resultBinding.Equals(wantBindings[i])
+		}
+
+		indexesOk := (len(wantIndexes) == len(resultIndexes))
+		for i, resultIndex := range resultIndexes {
+			indexesOk = indexesOk && resultIndex == wantIndexes[i]
+		}
+
+		if !bindingsOk || !indexesOk {
+			t.Errorf("MatchRelationToSet(%v %v %v): got %v %v, want %v %v", needle, haystack, binding, resultBindings, resultIndexes, wantBindings, wantIndexes)
 		}
 	}
 }

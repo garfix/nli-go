f2622c137266c529f38a7832f10bc7a051d2b66e
Patrick van Bergen <patrick.vanbergen@gmail.com>
2020-10-17 20:29:44 +0200

    space

--- tests/unit/FillerStack_test.go
+++ tests/unit/FillerStack_test.go
@@ -15,18 +15,18 @@ func TestFillerStack(t *testing.T) {
 
 	grammarRules := internalGrammarParser.CreateGrammarRules(`[
 
-		{ rule: s(P1) -> 'which' np(E1) dep_vp(P1, E1),			sense: which(E1) quant_check($np, $dep_vp) }
-		{ rule: np(E1) -> nbar(E1), 							sense: quant(_, some(_), E1, $nbar) }
+		{ rule: s(P1) -> 'which' np(E1) dep_vp(P1, E1),						sense: which(E1) quant_check($np, $dep_vp) }
+		{ rule: np(E1) -> nbar(E1), 										sense: quant(_, some(_), E1, $nbar) }
 		{ rule: nbar(E) -> noun(E) }
 		{ rule: dep_vp(P1, E1) -> be(_) np(E2) advp(P1) vp(P1, E1, E2), 	sense: quant_check($np, $advp $vp) }
-		{ rule: np(E1) -> qp(Q1) nbar(E1), 						sense: quant(Q1, $qp, E1, $nbar) }
+		{ rule: np(E1) -> qp(Q1) nbar(E1), 									sense: quant(Q1, $qp, E1, $nbar) }
 		{ rule: advp(P1) -> adverb(P1) }
-		{ rule: vp(P1, E1, E2) -> 'to' 'take' 'from', 			sense: take_from(P1, E2, E1)  }
+		{ rule: vp(P1, E1, E2) -> 'to' 'take' 'from', 						sense: take_from(P1, E2, E1)  }
 		{ rule: be(P1) -> 'were' }
-		{ rule: qp(Q1) -> 'the', sense: the(Q1) }
-		{ rule: adverb(A1) -> 'easiest', sense: easiest(A1) }
-		{ rule: noun(E1) -> 'babies', sense: baby(E1) }
-		{ rule: noun(E1) -> 'toys', sense: toy(E1) }
+		{ rule: qp(Q1) -> 'the', 											sense: the(Q1) }
+		{ rule: adverb(A1) -> 'easiest', 									sense: easiest(A1) }
+		{ rule: noun(E1) -> 'babies', 										sense: baby(E1) }
+		{ rule: noun(E1) -> 'toys',											sense: toy(E1) }
 	]`)
 
 	log := common.NewSystemLog(true)

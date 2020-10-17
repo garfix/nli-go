7723981193ca977f386c16e37498233270983cb0
Patrick van Bergen <patrick.vanbergen@gmail.com>
2020-10-17 20:09:00 +0200

    remove RelationGenerator

--- lib/central/RelationGenerator.go
+++
@@ -1,7 +0,0 @@
-package central
-
-import "nli-go/lib/mentalese"
-
-type RelationGenerator interface {
-	generate(template mentalese.Relation, bindings mentalese.Bindings) (mentalese.RelationSet, bool)
-}

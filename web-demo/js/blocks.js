5e0cd6d5bd8fc8dc83e44b5c0d2a3b94b4e8e50a
Patrick van Bergen <patrick.vanbergen@gmail.com>
2020-12-28 11:12:40 +0100

    skew!

--- web-demo/js/blocks.js
+++ web-demo/js/blocks.js
@@ -14,6 +14,16 @@ $(function(){
     const logBox = document.getElementById("log");
     const monitor = document.getElementById("monitor");
 
+    const colors = {
+        red: 0xc00000,
+        green: 0x008000,
+        blue: 0x0000c0,
+        white: 0xc0c0c0,
+        black: 0x654321
+    }
+
+    const opacity = 0.9;
+
     function setup()
     {
         popupCloseButton.onclick = function() {
@@ -162,25 +172,62 @@ $(function(){
     function buildScene(data)
     {
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        var camera = new THREE.OrthographicCamera( -7, 7, 7, -7, 0, 1000 );
+
+        const group = new THREE.Group();
 
         for (let i = 0; i < data.length; i++) {
             let datum = data[i];
             let object = createObject(datum);
-            scene.add(object);
+            skew(object)
+            group.add(createEdges(object))
+            group.add(object);
         }
+
+        scene.add( group );
 
         const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
         scene.add( directionalLight );
 
-        camera.position.set(5, 5, 7);
-        const renderer = new THREE.WebGLRenderer();
+        camera.position.set(6.75, 5, 0);
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setSize( 600, 300 );
 
         monitor.innerHTML = "";
         monitor.appendChild( renderer.domElement );
 
         renderer.render( scene, camera );
+    }
+
+    // https://stackoverflow.com/questions/36472653/drawing-edges-of-a-mesh-in-three-js
+    function createEdges(mesh)
+    {
+        var geometry = new THREE.EdgesGeometry( mesh.geometry );
+        var material = new THREE.LineBasicMaterial( { color: 0xf0f0f0 } );
+
+        return new THREE.LineSegments( geometry, material );
+    }
+
+    // https://stackoverflow.com/questions/26021618/how-can-i-create-an-axonometric-oblique-cavalier-cabinet-with-threejs
+    function skew(mesh)
+    {
+        var alpha = Math.PI / 4;
+
+        var Syx = 0,
+            Szx = - 0.5 * Math.cos( alpha ),
+            Sxy = 0,
+            Szy = - 0.5 * Math.sin( alpha ),
+            Sxz = 0,
+            Syz = 0;
+
+        var matrix = new THREE.Matrix4();
+
+        matrix.set(   1,   Syx,  Szx,  0,
+            Sxy,     1,  Szy,  0,
+            Sxz,   Syz,   1,   0,
+            0,     0,   0,   1  );
+
+        mesh.geometry.applyMatrix4(matrix);
     }
 
     function createObject(datum)
@@ -221,18 +268,9 @@ $(function(){
             new THREE.Face3( 0, 3, 4 )
         ];
 
-        let colors = {
-            red: 0xff0000,
-            green: 0x00ff00,
-            blue: 0x0000ff,
-            white: 0xffffff,
-            black: 0x808080
-        }
-
-        var material = new THREE.MeshBasicMaterial( {color: colors[datum.Color] , wireframe:false, transparent: true, opacity: 0.5} );
+        var material = new THREE.MeshBasicMaterial( {color: colors[datum.Color] , wireframe:false, transparent: true, opacity: opacity} );
         return new THREE.Mesh( geometry, material );
     }
-
 
     function createBlock(datum)
     {
@@ -268,8 +306,8 @@ $(function(){
             new THREE.Face3( 0, 3,  7),
             new THREE.Face3( 7, 4,  0),
 
-            new THREE.Face3( 6, 2,  1),
-            new THREE.Face3( 1, 5,  6),
+            new THREE.Face3( 1, 2,  6),
+            new THREE.Face3( 6, 5,  1),
 
             new THREE.Face3( 3, 2,  6),
             new THREE.Face3( 6, 7,  3),
@@ -278,15 +316,7 @@ $(function(){
             new THREE.Face3( 4, 7,  6),
         ];
 
-        let colors = {
-            red: 0xff0000,
-            green: 0x00ff00,
-            blue: 0x0000ff,
-            white: 0xffffff,
-            black: 0x808080
-        }
-
-        var material = new THREE.MeshBasicMaterial( {color: colors[datum.Color] , wireframe:false, transparent: true, opacity: 0.5} );
+        var material = new THREE.MeshBasicMaterial( {color: colors[datum.Color] , wireframe:false, transparent: true, opacity: opacity} );
         return new THREE.Mesh( geometry, material );
     }
 

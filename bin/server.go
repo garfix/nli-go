ac15c2e9b53ff022621033eb81cac37ddccd612e
Patrick van Bergen <patrick.vanbergen@gmail.com>
2022-03-12 08:47:41 +0100

    port

--- bin/server.go
+++ bin/server.go
@@ -1,9 +1,20 @@
 package main
 
-import "nli-go/lib/server"
+import (
+	"nli-go/lib/server"
+	"os"
+)
 
 func main() {
-	server := server.NewServer("3333")
+
+	if len(os.Args) != 2 {
+		println("Use: ./server <port>")
+		os.Exit(1)
+	}
+
+	port := os.Args[1]
+
+	server := server.NewServer(port)
 
 	server.Run()
 }
